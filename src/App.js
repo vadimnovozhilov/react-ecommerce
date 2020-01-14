66c7ae44194ee3decbe6074dd05d19cdc3e63d0d
vadimnovozhilov <novozhiloffvadim@gmail.com>
2020-01-14 21:09:47 +0300

    converted app.js into a class based component to utilize the fact of authentication across the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage.js';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './Pages/ShopPage/ShopPage';
 import Header from './Components/Header/Header.js';
 import Auth from './Pages/Auth/Auth';
+import { auth } from './firebase/utils.js';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={Auth} />
-      </Switch>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={Auth} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
