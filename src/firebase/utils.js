44ea5a60532c706833b171e159a6eb06fac5a335
vadimnovozhilov <novozhiloffvadim@gmail.com>
2020-01-15 21:22:03 +0300

    moved firebase config settings into a separate file

--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -1,16 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-const config = {
-    apiKey: "AIzaSyCsceLS3EVq-pFdOUm3pEG_71cN03KhY5s",
-    authDomain: "reactecomm.firebaseapp.com",
-    databaseURL: "https://reactecomm.firebaseio.com",
-    projectId: "reactecomm",
-    storageBucket: "reactecomm.appspot.com",
-    messagingSenderId: "181012660649",
-    appId: "1:181012660649:web:3ea02c10096db48fbac4b8"
-}
+import { config } from '../firebaseConfig.js';
 
 firebase.initializeApp(config);
 
